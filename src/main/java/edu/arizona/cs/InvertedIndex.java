428af3935ab145c2a0f577c81ee14470c73e5897
mithun <mithunpaul08@gmail.com>
2020-08-28 11:21:47 -0700

    renaming input.txt to Docs.txt

--- src/main/java/edu/arizona/cs/InvertedIndex.java
+++ src/main/java/edu/arizona/cs/InvertedIndex.java
@@ -1,15 +1,5 @@
 package edu.arizona.cs;
 
-import org.apache.lucene.document.Document;
-import org.apache.lucene.document.Field;
-import org.apache.lucene.document.StringField;
-import org.apache.lucene.document.TextField;
-import java.util.ArrayList;
-import java.util.List;
-import java.io.File;
-import java.io.IOException;
-import java.util.Scanner;
-
 public class InvertedIndex {
     boolean indexExists=false;
     String inputFilePath ="";
@@ -20,7 +10,7 @@ public class InvertedIndex {
 
     public static void main(String[] args ) {
         try {
-            String fileName = "input.txt";
+            String fileName = "Docs.txt";
             System.out.println("********Welcome to  Homework 1!");
             String query = "schizophrenia AND drug";
             InvertedIndex objInvertedIndex = new InvertedIndex(fileName);
